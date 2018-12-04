76dbdd039c2d1fa0973573637268996900c5f025
silso <chroelo@gmail.com>
2018-12-04 01:29:08 -0600

    created and joined threads, started implementing dispatch

--- server.c
+++ server.c
@@ -19,7 +19,7 @@
 #define BUFF_SIZE 1024
 
 /*
-  THE CODE STRUCTURE GIVEN BELOW IS JUST A SUGESSTION. FEEL FREE TO MODIFY AS NEEDED
+  THE CODE STRUCTURE GIVEN BELOW IS JUST A SUGESTION. FEEL FREE TO MODIFY AS NEEDED
 */
 
 // structs:
@@ -94,17 +94,26 @@ int getCurrentTimeInMills() {
 
 // Function to receive the request from the client and add to the queue
 void * dispatch(void *arg) {
+	char buf[BUF_SIZE];
+	while (1) {
 
-  while (1) {
-
-    // Accept client connection
-
-    // Get request from the client
-
-    // Add the request into the queue
-
-   }
-   return NULL;
+		// Accept client connection
+		int fd;
+		if ((fd = accept_connection()) < 0) {
+			//ignore
+		}
+		else {
+			// Get request from the client
+			if (get_request(fd, buf) != 0) {
+				printf("couldn't handle request for %s", buf);
+			}
+			else {
+				// Add the request into the queue
+				//lock
+			}
+		}
+	}
+	return NULL;
 }
 
 /**********************************************************************************/
@@ -142,6 +151,7 @@ int main(int argc, char **argv) {
   // Get the input args
 	char *in_port, *in_path, *in_num_dispatcher, *in_num_workers, *in_dynamic_flag, *in_queue_length, *in_cache_size;
 	int port, num_dispatcher, num_workers, dynamic_flag, queue_length, cache_size;
+	char *path; //not sure if this is right
 	in_port = argv[1];
 	in_path = argv[2];
 	in_num_dispatcher = argv[3];
@@ -152,23 +162,56 @@ int main(int argc, char **argv) {
 
   // Perform error checks on the input arguments
 	if ((port = atoi(in_port)) == 0) {
-		printf("bad port parameter: %s", in_port);
+		printf("bad port parameter: %s\n", in_port);
 		return -1;
 	}
 	struct stat test_path_stat;
 	stat(in_path, &test_path_stat);
 	if (S_ISDIR(test_path_stat.st.mode) == 0) {
-		printf("bad directory path: %s", in_path);
+		printf("bad directory path: %s\n", in_path);
+		return -1;
 	}
 	if ((num_dispatcher = atoi(in_num_dispatcher)) == 0) {
-		printf("bad num_dispatcher: %s", in_num_dispatcher);
+		printf("bad num_dispatcher: %s\n", in_num_dispatcher);
+		return -1;
 	}
+	else if (num_dispatcher > MAX_THREADS) {
+		printf("bad num_dispatcher: %d (max dispatch threads %d)\n", num_dispatcher, MAX_THREADS);
+		return -1;
+	}
+
+	// more checks here
+	
+	init(port);
 
   // Change the current working directory to server root directory
 
   // Start the server and initialize cache
 
   // Create dispatcher and worker threads
+	pthread_t dispatch_threads[num_dispatcher];
+	for (int i = 0; i < num_dispatcher; i++) {
+		if (pthread_create(&dispatch_threads[i], NULL, dispatch, NULL)) {
+			printf("error creating dispatcher thread %d\n", i);
+			return -1;
+		}
+		if (pthread_join(dispatch_threads[i], NULL)) {
+			printf("error joining dispatcher thread %d\n", i);
+			return -1;
+		}
+	}
+
+	pthread_t worker_threads[num_workers];
+	for (int i = 0; i < num_workers; i++) {
+		if (pthread_create(&worker_threads[i], NULL, worker, NULL)) {
+			printf("error creating worker thread %d\n", i);
+			return -1;
+		}
+		if (pthread_join(worker_threads[i], NULL)) {
+			printf("error joining worker thread %d\n", i);
+			return -1;
+		}
+	}
 
   // Clean up
   return 0;
